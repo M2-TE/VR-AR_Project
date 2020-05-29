13fcc92991f35751490c1572acdd6919db4aa440
YDenker <33430355+YDenker@users.noreply.github.com>
2020-05-29 22:32:12 +0200

    add Navigation Proto

---
+++ VR Navigation Oculus/Assets/CharacterMovement/Scripts/VRController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRController : MonoBehaviour
+{
+    public OVRManager ovrManager;
+
+    void Start()
+    {
+        ovrManager.RecenterPlayer();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || OVRInput.GetDown(OVRInput.Button.Start))
+            ovrManager.RecenterPlayer();
+    }
+}
